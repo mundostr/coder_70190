799e355aee3939f66a4cc4dba589016807655455
Carlos Perren <idux.net@gmail.com>
2024-11-13 08:33:20 -0300

    Variables de entorno y opcs línea de comandos

--- class_8/src/config.js
+++ class_8/src/config.js
@@ -1,14 +1,41 @@
 import * as url from 'url';
 import { Command } from 'commander';
+import dotenv from 'dotenv';
 
+/**
+ * OPCIONES DE LINEA DE COMANDOS
+ * El módulo commander nos permite capturar fácilmente configuraciones
+ * desde la línea de comandos al ejecutar el script.
+ * 
+ * A través del método option() podemos definir todas las que necesitemos.
+ */
 const commandLine = new Command();
 commandLine
     .option('--mode <mode>')
     .option('--port <port>')
 commandLine.parse();
+// clOptions contendrá un objeto con todas las opciones que se hayan pasado
 const clOptions = commandLine.opts();
-console.log(clOptions);
 
+
+/**
+ * VARIABLES DE ENTORNO
+ * NO utilizar dotenv y la opción --env-file de node a la vez, elegir una u otra.
+ * 
+ * El método config() de dotenv, toma lo definido en el archivo y lo envía
+ * al sistema operativo para que sea agregado al paquete de variables de entorno.
+ * A partir de allí, estará disponible en process.env.
+ */
+dotenv.config({ path: '.env' });
+// Podríamos indicar con una opción de línea de comandos si queremos configurar
+// en modo desarrollo o producción
+// dotenv.config({ path: clOptions.mode === 'devel' ? '.env.devel' : '.env.prod' });
+
+/**
+ * Podemos ver que el objeto config() ya no tiene datos sensibles hardcodeados,
+ * los recupera desde variables de entorno (como process.env.PORT) u opciones de
+ * línea de comandos (como clOptions.port).
+ */
 const config = {
     APP_NAME: 'coder70190',
     PORT: process.env.PORT || clOptions.port || 8080,
